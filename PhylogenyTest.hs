a7745a6e39bed54f674e133a20558b181b4a25c5
Florian Eggenhofer <egg@tbi.univie.ac.at>
2014-09-22 10:03:29 +0200

    Added testing files

---
+++ PhylogenyTest.hs
@@ -0,0 +1,22 @@
+-- | Parser test script
+-- /scr/kronos/egg/ghc/ghc/bin/runghc PhylogenyTest.hs query2.dnd
+
+module Main where
+    
+import System.Environment (getArgs)
+import System.Process 
+import Text.ParserCombinators.Parsec
+import System.IO
+import System.Environment
+import Data.List
+import Bio.Phylogeny
+import System.Directory
+import System.Process
+import Control.Monad    
+import Data.Either.Unwrap
+    
+main = do
+  args <- getArgs
+  let input_file = (head args)
+  phylogeny <- readNewick input_file
+  print phylogeny
